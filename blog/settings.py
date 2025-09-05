c105345c9295a5c6ff5a0a1c51db182c5da8bd79
nico-sauer <nico.sauer@dci-student.org>
2025-09-05 09:32:03 +0200

    fixed static file path

--- blog/settings.py
+++ blog/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
+#STATICFILES_DIRS = [
+    #BASE_DIR / "staticfiles",
     # '/var/www/static/',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR/"staticfiles")
